1089a36e0889f6f6a902e799a1f8a45659e9c013
Xinchen Zhou <zxc@MacBook-Pro.local>
2024-07-30 19:38:41 +1000

    data fetching

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+const nav = [
+  {
+    label: 'Home',
+    to: '/',
+  },
+  {
+    label: 'External',
+    to: '/external',
+  },
+  {
+    label: 'Component',
+    to: '/component',
+  },
+]
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtExample dir="features/data-fetching" :nav="nav">
+    <!-- Show Nuxt progress indicator on page change -->
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  </NuxtExample>
 </template>
+
+
+<!-- learn abt https://nuxt.com/docs/examples/features/data-fetching -->
