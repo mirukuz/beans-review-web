c1c14c2d74e00247401da4ab96a787b13f4dd60e
Xinchen Zhou <zxc@MacBook-Pro.local>
2024-07-31 17:17:51 +1000

    setup graphql

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  extends: [
-    '@nuxt/examples-ui'
-  ],
-})
+
+  extends: ["@nuxt/examples-ui"],
+
+  modules: ["nuxt-graphql-request", "nuxt-graphql-request"],
+  build: {
+    transpile: ["nuxt-graphql-request"],
+  },
+
+  graphql: {
+    clients: {
+      default: {
+        endpoint: "http://127.0.0.1:4000",
+        options: {},
+      },
+    },
+  },
+});
--- pages/external.vue
+++ pages/external.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
-const id = ref(1)
-const { data: product, pending, error } = await useFetch(() => `https://dummyjson.com/products/${id.value}`)
-
+import { gql } from 'nuxt-graphql-request/utils';
+const { $graphql } = useNuxtApp();
+// import { useAuth0 } from '@auth0/auth0-vue'
+const query = gql`
+query planets {
+  feed {
+    id
+    title
+    content
+    published
+    author {
+      id
+      name
+      email
+    }
+  }
+}
+`;
+// const id = ref(1)
+// const { isAuthenticated, isLoading, user, loginWithRedirect, logout } = useAuth0()
+// const { data: product, pending, error } = await useFetch(() => `https://dummyjson.com/products/${id.value}`)
+const { data } = await useAsyncData('planets', async () => {
+  const data = await $graphql.default.request(query);
+  return data;
+});
 /* Same as:
 const { data: product, pending, error } = await useAsyncData(() => {
   return $fetch(`https://dummyjson.com/products/${id.value}`)
@@ -9,13 +31,28 @@ const { data: product, pending, error } = await useAsyncData(() => {
   watch: [id]
 })
 */
+// const login = async () => {
+//   try {
+//     await loginWithRedirect()
+//   } catch (error) {
+//     console.error('Login failed:', error)
+//   }
+// }
+// https://stackoverflow.com/questions/74477187/how-to-implement-auth0-with-nuxt3
 
-const canDecrease = computed(() => id.value > 1)
+// const canDecrease = computed(() => id.value > 1)
 </script>
 
 <template>
   <div class="flex flex-col gap-2">
-    <p class="flex items-center gap-2">
+    <pre>
+      <code>DATA: {{ data }}</code>
+    </pre>
+    <!-- <pre v-if="isAuthenticated">
+      <code>USER: {{ user }}</code>
+    </pre>
+    <button @click="login">login</button> -->
+    <!-- <p class="flex items-center gap-2">
       Result of <code>https://dummyjson.com/products/</code>
       <UInput type="number" v-model="id" />
     </p>
@@ -26,7 +63,7 @@ const canDecrease = computed(() => id.value > 1)
     </p>
     <p v-if="pending">Fetching...</p>
     <pre v-else-if="error">{{ error }}</pre>
-    <pre v-else>{{ product }}</pre>
+    <pre v-else>{{ product }}</pre> -->
     <NuxtLink class="underline" to="/">Back to Home</NuxtLink>
   </div>
 </template>
