1c5ab70918fa09402cbd5c8ea6b8bde65901b4c4
zxc <xinczhou@gmail.com>
2024-08-25 20:56:57 +1000

    update gql host in the env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       Auth0ClientId: process.env.AUTH0_CLIENT_ID,
       Auth0Domain: process.env.AUTH0_DOMAIN,
       Auth0RedirectUri: process.env.AUTH0_REDIRECT_URI,
-      GQL_HOST: 'http://127.0.0.1:4000'
+      GQL_HOST: process.env.GQL_HOST
     }
   },
   modules: ["nuxt-graphql-client", "@nuxt/ui"],
